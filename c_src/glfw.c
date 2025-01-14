ab766128079e8eaa8a791b6ee930540e4cfb84c7
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-01-14 17:06:08 +0800

    Implement NIF command executor thread
    Because GLFW is not thread-safe and most of its functions are designed to run from the main thread (read 'same thread', not necessarily 'main thread'), the solution is to implement a NIF function executor thread.

--- c_src/glfw.c
+++ c_src/glfw.c
@@ -8,10 +8,78 @@
 // Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>, January 2025
 //
 #include <string.h>
+#include <pthread.h>
 #include <erl_nif.h>
 #include <GLFW/glfw3.h>
 
+static pthread_t commands_executor;
+static pthread_mutex_t command_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t command_ready = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t command_done = PTHREAD_COND_INITIALIZER;
+
+static ERL_NIF_TERM (*command_function)(ErlNifEnv*, int, const ERL_NIF_TERM[]) = NULL;
+static ErlNifEnv* command_args_1 = NULL;
+static int command_args_2 = 0;
+static ERL_NIF_TERM** command_args_3 = NULL;
+static ERL_NIF_TERM command_result;
+
+static int command_finished = 0;
+
 static ErlNifResourceType* glfw_monitor_resource_type = NULL;
+
+// The function of the thread that executes "NIF commands". It just waits for a
+// command to be ready, executes it and signals that the command is done (while
+// making the result available).
+void* commands_executor_function(void* arg) {
+    // XXX: Improve the implementation. For now it does the job.
+    while (1) {
+        pthread_mutex_lock(&command_mutex);
+        while (command_function == NULL) {
+            pthread_cond_wait(&command_ready, &command_mutex);
+        }
+
+        command_result = command_function(
+            command_args_1,
+            command_args_2,
+            (const ERL_NIF_TERM**)command_args_3
+        );
+
+        command_function = NULL;
+        command_finished = 1;
+        pthread_cond_signal(&command_done);
+        pthread_mutex_unlock(&command_mutex);
+    }
+
+    return NULL;
+}
+
+// It executes a "NIF command". It schedules the command to be executed by the
+// commands executor thread, waits for the command to be executed and returns
+// the result of the command.
+ERL_NIF_TERM execute_command(
+    ERL_NIF_TERM (*function)(ErlNifEnv*, int, const ERL_NIF_TERM[]),
+    ErlNifEnv* env,
+    int argc,
+    ERL_NIF_TERM* argv[]
+) {
+    pthread_mutex_lock(&command_mutex);
+    command_function = function;
+    command_args_1 = env;
+    command_args_2 = argc;
+    command_args_3 = argv;
+    command_finished = 0;
+
+    pthread_cond_signal(&command_ready);
+
+    while (!command_finished) {
+        pthread_cond_wait(&command_done, &command_mutex);
+    }
+
+    ERL_NIF_TERM result = command_result;
+    pthread_mutex_unlock(&command_mutex);
+
+    return result;
+}
 
 static void glfw_monitor_resource_dtor(ErlNifEnv* env, void* obj) {
 }
@@ -21,6 +89,12 @@ static int nif_module_load(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM arg)
     glfw_monitor_resource_type = enif_open_resource_type(env, NULL, "glfw_monitor", glfw_monitor_resource_dtor, ERL_NIF_RT_CREATE, NULL);
     if (glfw_monitor_resource_type == NULL) {
         fprintf(stderr, "failed to open 'GLFW monitor' resource type\n");
+        return -1;
+    }
+
+    // Start the "NIF commands" executor thread.
+    if (pthread_create(&commands_executor, NULL, commands_executor_function, NULL) != 0) {
+        fprintf(stderr, "failed to create the commands executor thread\n");
         return -1;
     }
 
